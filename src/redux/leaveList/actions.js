4d83702d671a2edf6bc1e677163433dbae241d93
Danang Nugroho <danang.is@tnis.com>
2019-10-15 19:04:23 +0700

    Add

--- src/redux/leaveList/actions.js
+++ src/redux/leaveList/actions.js
@@ -1,7 +1,10 @@
 import {
     LEAVE_LIST_GET_PENDING_LIST,
     LEAVE_LIST_GET_PENDING_LIST_SUCCESS,
-    LEAVE_LIST_GET_PENDING_LIST_ERROR
+    LEAVE_LIST_GET_PENDING_LIST_ERROR,
+    LEAVE_LIST_GET_APPROVED_LIST,
+    LEAVE_LIST_GET_APPROVED_LIST_SUCCESS,
+    LEAVE_LIST_GET_APPROVED_LIST_ERROR
 } from '../actions';
 
 
@@ -16,5 +19,19 @@ export const getLeavePendingListSuccess = (items) => ({
 
 export const getLeavePendingListError = (error) => ({
     type: LEAVE_LIST_GET_PENDING_LIST_ERROR,
+    payload: error
+});
+
+export const getLeaveApprovedList = () => ({
+    type: LEAVE_LIST_GET_APPROVED_LIST
+});
+
+export const getLeaveApprovedListSuccess = (items) => ({
+    type: LEAVE_LIST_GET_APPROVED_LIST_SUCCESS,
+    payload: items
+});
+
+export const getLeaveApprovedListError = (error) => ({
+    type: LEAVE_LIST_GET_APPROVED_LIST_ERROR,
     payload: error
 });
